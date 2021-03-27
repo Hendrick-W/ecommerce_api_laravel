3958a1b1b1c367794a0c2fe83cb0a7cced91db02
Hendrick <hendrickwijaya21@gmail.com>
2021-03-27 20:30:25 +0700

    Add seeder with faker

--- database/factories/ReviewsFactory.php
+++ database/factories/ReviewsFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Reviews;
+use App\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Reviews::class, function (Faker $faker) {
     return [
         //
+        'product_id' => function() {
+            return Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5),
+
     ];
 });
